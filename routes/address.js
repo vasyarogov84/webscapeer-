82a842817f798fb7b295eeefb76b32135c5a338e
Viktor Grom <viktorgrom@lithia.com>
2020-12-27 22:19:47 -0800

    zillow

--- routes/address.js
+++ routes/address.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const scrapeByAddress = require("../utils/scrapeByAddress");
 
 const url =
-  "https://www.zillow.com/homedetails/123-Dormont-Dr-NE-Huntsville-AL-35810/109521302_zpid/";
+  "https://www.zillow.com/homedetails/3816-Colton-Ln-NE-Huntsville-AL-35811/92116615_zpid/";
 
 /* GET home page. */
 router.get("/", async (req, res, next) => {
